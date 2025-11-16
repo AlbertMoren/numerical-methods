b3b3de8d818024e4c8775c9b95755e361173a210
= <gustavomartinsjs@alu.ufc.br>
2025-11-16 19:02:36 -0300

    fix: Função derivada com parâmetro a adicionado.

--- metodos/newton_raphson/newton_raphson.cpp
+++ metodos/newton_raphson/newton_raphson.cpp
@@ -17,36 +17,41 @@ esse deslocamento d da extremidade de um jato supersônico desse tipo considerad
 #include <iostream>
 #include <cmath>
 #include <utility>
+#include <stdexcept>
 using namespace std;
 
-double f(float d, float a){
+double f(double d, double a){
     return a*d-(d*log(d));
 }
 
-double derivada(double (*func)(float, float), double d, double h){
-    return (func(d + h, 1) - func(d - h, 1)) / (2*h); // f'(d) = -ln(d)
+double derivada(double (*func)(double, double), double d, double h, double a){
+    return (func(d + h, a) - func(d - h, a)) / (2*h); // f'(d) = a -ln(d) - 1
 }
 
-pair<double, int> Newton_Raphson(float d, float a, float e){
+pair<double, int> Newton_Raphson(double d, double a, double e){
     int passos=0;
-    while(true){
+    while(passos < 100){
         double f_d = f(d,a);
-        double d_dd = derivada(f,d,e);
+        double d_dd = derivada(f,d,e,a);
         double d_i = d - (f_d/d_dd);
         passos++;
         double erro = fabs(d-d_i);
         if (erro < e) return make_pair(d_i, passos);
         d = d_i;
     }
+    throw runtime_error("O método de Newton-Raphson não convergiu dentro do limite de iterações.");
 }
 
 int main()
 {
-    float d_0 = 2.5; // "chute inicial"
-    float a = 1.0; 
-    float e = 0.00001; // erro
+    double d_0 = 2.5; // "chute inicial" ((2+3)/2)
+    double a = 1.0; 
+    double e = 0.00001; // erro
     pair<double, int> res= Newton_Raphson(d_0,a,e);
+    cout << "===============================================================================" << endl;
+    cout << "Teste com a = " << a << ", d_0 = " << d_0 << " e erro = " << e << "." << endl;
     cout << "Resultado: " << res.first << endl;
     cout << "Quantidade de passos necessários: " <<res.second << endl;
+    cout << "===============================================================================" << endl;
     return 0;
 }
